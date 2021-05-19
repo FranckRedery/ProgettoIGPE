d9452b066031074c72bd83b92f9d80bcc55af159
Francesco Reda <62659515+FranckRedery@users.noreply.github.com>
2021-05-19 13:56:32 +0200

    Add files via upload

--- GameLoop.java
+++ GameLoop.java
@@ -6,15 +6,17 @@ public class GameLoop extends Thread{
 
 	public AnimationController controller;
 	public GameLoop(AnimationController controller) {
-		this.controller = controller;
+		this.controller = controller;	
 	}
 	
 	@Override
 	public void run() {
 		super.run();
+		//long avvio = System.currentTimeMillis(); controllo del tempo , non dovrebbe servire se creo un altro thread
 		while(true) {
 			try {
 				controller.update();
+				//System.out.println(System.currentTimeMillis()-avvio);
 				Thread.sleep(60);
 			} catch (InterruptedException e) {
 				return;
