337b2d7731c6f137a10ee12cb8c7b24ebecb47fb
Francesco Reda <62659515+FranckRedery@users.noreply.github.com>
2021-05-20 17:29:47 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,11 +3,18 @@ package application;
 import javax.swing.JFrame;
 
 import application.controller.AnimationController;
+import application.model.Game;
 import application.view.GraphicPanel;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
+		// TO DO MENU  //
+		
+		
+		
+		
 		JFrame f = new JFrame("Prova");
 		f.setSize(Settings.WINDOW_SIZE,Settings.WINDOW_SIZE);		
 		GraphicPanel gp = new GraphicPanel();
@@ -18,6 +25,8 @@ public class Main {
 		GameLoop gameLoop = new GameLoop(controller);
 		gameLoop.start();
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setVisible(true);		
+		f.setLocationRelativeTo(null);
+		f.setUndecorated(true);
+		f.setVisible(true);	
 	}
 }
--- view/GraphicPanel.java
+++ view/GraphicPanel.java
@@ -1,10 +1,17 @@
 package application.view;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontFormatException;
 import java.awt.Graphics;
+import java.awt.GraphicsEnvironment;
+import java.io.File;
+import java.io.IOException;
 
 import javax.swing.JPanel;
 
 import application.Settings;
+import application.model.Game;
 import application.model.MyCharacter;
 
 public class GraphicPanel extends JPanel {
@@ -13,12 +20,26 @@ public class GraphicPanel extends JPanel {
 	private myCharacterAnimations animations = new myCharacterAnimations(); 
 	
 
-	public GraphicPanel() {}
+	public GraphicPanel() {
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		try {
+			ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("Font/Fiendish.ttf")));
+		} catch (FontFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		this.setBackground(Color.black);
+	}
 
 	@Override
 	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);		
-		g.drawImage(animations.getCurrentImage(), MyCharacter.x, MyCharacter.y, Settings.BLOCK_DIM, Settings.BLOCK_DIM, null);
+		super.paintComponent(g);
+		g.drawImage(animations.getCurrentImage(), Game.getInstance().getCharacter().x,Game.getInstance().getCharacter().y , Settings.BLOCK_DIM, Settings.BLOCK_DIM, null);
+		g.setColor(Color.RED);
+		g.setFont(new Font("Fiendish",Font.PLAIN,20));
+		g.drawString("Uccisi " , 200, 100);
+		
 	}
 	
 	public void updateAnimation(int type) {
@@ -30,3 +51,4 @@ public class GraphicPanel extends JPanel {
 		repaint();
 	}
 }
+
